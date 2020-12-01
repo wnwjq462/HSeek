1fe784a946c50a572f2b81b6da8efa4d9a431354
wnwjq462 <74763242+wnwjq462@users.noreply.github.com>
2020-12-01 22:02:08 +0900

    Update scrapping_test.py
    Fill new DB with merged data from code list in old DB

--- scrapping_test.py
+++ scrapping_test.py
@@ -8,95 +8,108 @@ import numpy as np
 import sqlite3
 import time
 
+
 total_list = []
 range_list = [5,6,7,8,9,13,14,15,16,17,21,22,23,24,25,29,30,31,32,33]
-
-past_date = "0"
-code = "000040"
-t = 1
-
 con = sqlite3.connect("c:/Users/H/Desktop/HTrader/HSeek/kospi_daydata.db")
 con_2 = sqlite3.connect("c:/Users/H/Desktop/HTrader/HSeek/kospi_daydata_2.db")
-table_name = "_" + code
-sql = "SELECT * FROM "
-sql = sql + "_" + code
-df = pd.read_sql(sql, con, index_col=None)
-start_time = time.time()
-while True :
 
-    print(t)
-    url = "https://finance.naver.com/item/frgn.nhn?code="
-    url = url + code + "&page=" + str(t)
-    t+=1
-    #if t == 23 : break
-    #print(url)
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
+sql = "SELECT name FROM sqlite_master WHERE type = 'table';"
+code_list = pd.read_sql(sql, con)
+code_list = code_list.values.tolist()
+code_list = [x.replace('_', '') for x in np.reshape(code_list, -1)]
 
-    title = soup.select(
-     '#content > div.section.inner_sub > table.type2 > tr:nth-child(5) > td.tc > span'
-    )
-    cur_date = title[0].text
+for code in code_list[0:4] :
+    past_date = "0"
+    #code = "000040"
+    print(code)
+    t = 1
 
-    if past_date == cur_date : break
+    table_name = "_" + code
+    sql = "SELECT * FROM "
+    sql = sql + "_" + code
+    df = pd.read_sql(sql, con, index_col=None)
+    start_time = time.time()
+    while True:
 
-    past_date = title[0].text
-    list = [ [0]*3 for i in range(20)]
+        print(t,end=" ")
+        url = "https://finance.naver.com/item/frgn.nhn?code="
+        url = url + code + "&page=" + str(t)
+        t += 1
+        # if t == 23 : break
+        # print(url)
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, 'html.parser')
 
-    for i in range_list :
-        index = 0
-        if int(i/5) == 1 : index = i-5
-        elif int((i-3)/5) == 2 : index = i-8
-        elif int((i-1)/5) == 4 : index = i-11
-        else : index = i-14
-
-        selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
-        selector = selector + str(i) +  ') > td.tc > span'
         title = soup.select(
-            selector
+            '#content > div.section.inner_sub > table.type2 > tr:nth-child(5) > td.tc > span'
         )
-
         if len(title) == 0 : break
+        cur_date = title[0].text
 
-        cur_date = title[0].text.replace(".","")
-        list[index][0] = cur_date
+        if past_date == cur_date: break
 
-        selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
-        selector = selector + str(i) + ') > td:nth-child(6) > span'
-        title = soup.select(
-            selector
-        )
+        past_date = title[0].text
+        list = [[0] * 3 for i in range(20)]
 
-        list[index][1] = title[0].text
+        for i in range_list:
+            index = 0
+            if int(i / 5) == 1:
+                index = i - 5
+            elif int((i - 3) / 5) == 2:
+                index = i - 8
+            elif int((i - 1) / 5) == 4:
+                index = i - 11
+            else:
+                index = i - 14
 
-        selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
-        selector = selector + str(i) + ') > td:nth-child(7) > span'
-        title = soup.select(
-            selector
-        )
+            selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
+            selector = selector + str(i) + ') > td.tc > span'
+            title = soup.select(
+                selector
+            )
 
-        list[index][2] = title[0].text
+            if len(title) == 0: break
 
+            cur_date = title[0].text.replace(".", "")
+            list[index][0] = cur_date
 
-        #print(list[index][0] + " " + list[index][1] + " " + list[index][2])
+            selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
+            selector = selector + str(i) + ') > td:nth-child(6) > span'
+            title = soup.select(
+                selector
+            )
 
+            list[index][1] = title[0].text
 
-        #print(pf)
+            selector = '#content > div.section.inner_sub > table.type2 > tr:nth-child('
+            selector = selector + str(i) + ') > td:nth-child(7) > span'
+            title = soup.select(
+                selector
+            )
 
-    total_list += list
+            list[index][2] = title[0].text
 
+            # print(list[index][0] + " " + list[index][1] + " " + list[index][2])
+
+            # print(pf)
+
+        total_list += list
+    print("")
+
+    pf = DataFrame(total_list, columns=['날짜', '기관', '외국인'])
+    #print(pf)
+
+    merge_df = pd.merge(df, pf, how='inner', on='날짜')
+    #print(merge_df)
+    end_time = time.time()
+    print("total time : ", (end_time - start_time))
+    merge_df.to_sql(table_name, con_2, if_exists="replace", index=False)
 
 
 
 
 #total_list = total_list[0:8]
-pf = DataFrame(total_list, columns=['날짜','기관','외국인'])
-print(pf)
 
-merge_df = pd.merge(df,pf,how = 'inner', on = '날짜')
-print(merge_df)
-end_time = time.time()
-print("total time : ",(end_time - start_time))
-merge_df.to_sql(table_name,con_2,if_exists="replace", index = False)
 con_2.commit()
 con_2.close()
